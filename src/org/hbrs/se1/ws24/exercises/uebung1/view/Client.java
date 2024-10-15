8eddfc07f475a2c0b29eae8a643971ae25ca0bca
ronib <roni.yalci@smail.inf.h-brs.de>
2024-10-15 22:18:11 +0200

    Klasse erstellt ohne Methode

--- src/org/hbrs/se1/ws24/exercises/uebung1/view/Client.java
+++ src/org/hbrs/se1/ws24/exercises/uebung1/view/Client.java
@@ -1,26 +1,38 @@
 package org.hbrs.se1.ws24.exercises.uebung1.view;
+import org.hbrs.se1.ws24.exercises.uebung1.control.*;
+import org.hbrs.se1.ws24.exercises.uebung1.control.translatorFactory.*;
 
 public class Client {
 
-		/**
-		 * Methode zur Ausgabe einer Zahl auf der Console
-		 * (auch bezeichnet als CLI, Terminal)
-		 *
-		 */
-		 void display( int aNumber ){
-			// In dieser Methode soll die Methode translateNumber
-			// mit dem übergegebenen Wert der Variable aNumber
-			// aufgerufen werden.
-			//
-			// Strenge Implementierung (nur) gegen das Interface Translator gewuenscht!
+	private Translator translator;
 
-			 System.out.println("Das Ergebnis der Berechnung: " +
-					"[das Ergebnis an dieser Stelle]" );
+	public Client(Translator trs){
+		this.translator = trs;
+	}
 
-		 }
+	public void setTranslator(Translator translator) {
+		this.translator = translator;
+	}
+
+	/**
+	 * Methode zur Ausgabe einer Zahl auf der Console
+	 * (auch bezeichnet als CLI, Terminal)
+	 *
+	 */
+
+
+	public static void display( int aNumber ){
+        /* In dieser Methode soll die Methode translateNumber
+        mit dem übergegebenen Wert der Variable aNumber
+        aufgerufen werden.
+        Strenge Implementierung (nur) gegen das Interface Translator gewuenscht! */
+
+		Translator trs = TranslatorFactory.generateGermanTranslator();
+		String ausgabe = trs.translateNumber(aNumber);
+
+		System.out.println("Das Ergebnis der Berechnung: " +
+				aNumber + " -> " + ausgabe);
+
+	}
+
 }
-
-
-
-
-
