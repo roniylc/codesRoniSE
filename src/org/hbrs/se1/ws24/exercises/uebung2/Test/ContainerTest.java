82d242f690f08ab6e5d929ddfd7eb1ed49875834
ronib <roni.yalci@smail.inf.h-brs.de>
2024-10-15 23:20:45 +0200

    Fertige Version

--- src/org/hbrs/se1/ws24/exercises/uebung2/Test/ContainerTest.java
+++ src/org/hbrs/se1/ws24/exercises/uebung2/Test/ContainerTest.java
@@ -17,7 +17,6 @@ public class ContainerTest {
         c.addMember(m2);
     }
 
-    // Testet addMember(Member) methode
     @Test
     void test1() throws ContainerException {
         // pos addMember() test
@@ -25,45 +24,73 @@ public class ContainerTest {
         c.addMember(m);
         assertEquals(3,c.size());
         // neg addMember() test --> mit gleicher ID
-        Member m4 = new ConcreteMember(2);
+        Member m4 = new ConcreteMember(1);
         ContainerException thrown = assertThrows(ContainerException.class, () -> { c.addMember(m4);});
-        assertEquals("Das Member-Objekt mit der ID 2 ist bereits\nvorhanden!", thrown.getMessage());
+        assertEquals("Das Member-Objekt mit der ID 1 ist bereits\nvorhanden!", thrown.getMessage());
 
     }
 
-    // Testet deleteMember(int) methode
     @Test
     void test2() throws ContainerException {
         // pos deleteMember() test
-        Member m3 = new ConcreteMember(3);
-        c.deleteMember(3);
-        assertEquals(2,c.size());
-        // neg deleteMember() test mit falscher ID oder wenn es keine Member gibt
-        c.deleteMember(20);
-        assertEquals("Member-Objekt mit der ID = " + 20 + " existiert nicht", c.deleteMember(20));
-    }
-
-    // delete wenn keine Member, Wenn man null übergibt, Mit mehreren membern bei add und delete, Container leer beim erzeugen,
-
-    // testet dump() methode
-    @Test
-    void test3() {
-
-    }
-
-    // testet size() methode
-    @Test
-    void test4() throws ContainerException {
-        // pos size() test
-        assertEquals(2,c.size());
-        // pos size() nach addMember(Member)
-        Member m1 = new ConcreteMember(3);
-        c.addMember(m1);
-        assertEquals(3,c.size());
-        // pos size() test nach deleteMember(int)
-        c.deleteMember(2);
         c.deleteMember(1);
         assertEquals(1,c.size());
+        // neg deleteMember() test mit falscher ID oder wenn es keine Member gibt
+        c.deleteMember(3);
+        assertEquals("Member-Objekt mit der ID = " + 3 + " existiert nicht", c.deleteMember(3));
     }
+
+    @Test
+    void test3(){
+        Container c2 = new Container();
+        c2.deleteMember(1);
+        assertEquals("Member-Objekt mit der ID = " + 1 + " existiert nicht", c2.deleteMember(1));
+    }
+
+    @Test
+    void test4() throws ContainerException {
+        Member m3 = new ConcreteMember(3);
+        Member m4 = new ConcreteMember(4);
+        Member m5 = new ConcreteMember(5);
+        c.addMember(m3);
+        c.addMember(m4);
+        c.addMember(m5);
+        assertEquals(5,c.size());
+    }
+
+    @Test
+    void test5() throws ContainerException {
+        Container c2 = new Container();
+        Member m3 = new ConcreteMember(3);
+        Member m4 = new ConcreteMember(4);
+        Member m5 = new ConcreteMember(5);
+        c2.addMember(m3);
+        c2.addMember(m4);
+        c2.addMember(m5);
+        c2.deleteMember(3);
+        c2.deleteMember(4);
+        c2.deleteMember(5);
+        assertEquals(0,c2.size());
+    }
+
+    @Test
+    void test6() throws ContainerException {
+        Member m3 = new ConcreteMember(3);
+        c.addMember(m3);
+        c.deleteMember(3);
+        assertEquals(2,c.size());
+    }
+
+    @Test
+    void test7() {
+        Container c2 = new Container();
+        assertEquals(0,c2.size());
+    }
+
+    /*@Test
+    void test8() throws ContainerException {
+        c.addMember(null);
+        assertEquals(2,c.size());
+    }*/
 
 }
