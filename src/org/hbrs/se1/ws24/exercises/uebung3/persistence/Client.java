bed6e1f9fb242086e6f9e77ba01d8584564fde62
ronib <roni.yalci@smail.inf.h-brs.de>
2024-10-21 21:39:29 +0200

    Vorläufige Version

---
+++ src/org/hbrs/se1/ws24/exercises/uebung3/persistence/Client.java
@@ -0,0 +1,33 @@
+package org.hbrs.se1.ws24.exercises.uebung3.persistence;
+import org.hbrs.se1.ws24.exercises.uebung2.ConcreteMember;
+import org.hbrs.se1.ws24.exercises.uebung2.Container;
+import org.hbrs.se1.ws24.exercises.uebung2.ContainerException;
+import org.hbrs.se1.ws24.exercises.uebung2.Member;
+
+import java.util.List;
+
+public class Client {
+
+    public static void main(String[] args) {
+        try{
+            Container c = Container.getInstance();
+
+            Member m1 = new ConcreteMember(1);
+            Member m2 = new ConcreteMember(2);
+            Member m3 = new ConcreteMember(3);
+
+            c.addMember(m1);
+            c.addMember(m2);
+            c.addMember(m3);
+
+            List<Member> current = c.getCurrentList();
+
+            MemberView v = new MemberView();
+
+            v.dump(current);
+        } catch (ContainerException e){
+            System.out.println("Fehler: " + e.getMessage());
+        }
+    }
+
+}
--- src/org/hbrs/se1/ws24/exercises/uebung3/persistence/PersistenceStrategyStream.java
+++ src/org/hbrs/se1/ws24/exercises/uebung3/persistence/PersistenceStrategyStream.java
@@ -1,6 +1,5 @@
 package org.hbrs.se1.ws24.exercises.uebung3.persistence;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.util.List;
 
 public class PersistenceStrategyStream<E> implements PersistenceStrategy<E> {
@@ -17,12 +16,18 @@ public class PersistenceStrategyStream<E> implements PersistenceStrategy<E> {
     @Override
     /**
      * Method for saving a list of Member-objects to a disk (HDD)
-     * Look-up in Google for further help! Good source:
-     * https://www.digitalocean.com/community/tutorials/objectoutputstream-java-write-object-file
-     * (Last Access: Oct, 15th 2024)
+     * Look-up in Google for further help!
      */
     public void save(List<E> member) throws PersistenceException  {
-
+        try {
+            FileOutputStream fos = new FileOutputStream(location);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(member); // schreibt Objekt in File
+            oos.close();
+            fos.close();
+        } catch (IOException e) {
+            throw new PersistenceException(PersistenceException.ExceptionType.ConnectionNotAvailable,"Fehler beim Speichern der Daten: " + e.getMessage());
+        }
     }
 
     @Override
@@ -32,26 +37,25 @@ public class PersistenceStrategyStream<E> implements PersistenceStrategy<E> {
      * Take also a look at the import statements above ;-!
      */
     public List<E> load() throws PersistenceException  {
-        // Some Coding hints ;-)
+        try {
+            FileInputStream fis = new FileInputStream(location);
+            ObjectInputStream ois = new ObjectInputStream(fis);
 
-        // ObjectInputStream ois = null;
-        // FileInputStream fis = null;
-        // List<...> newListe =  null;
-        //
-        // Initiating the Stream (can also be moved to method openConnection()... ;-)
-        // fis = new FileInputStream( " a location to a file" );
+            Object obj = ois.readObject();
+            if(obj instanceof List<?>) {
+                return (List<E>) obj;
+            } else {
+                throw new PersistenceException(PersistenceException.ExceptionType.ImplementationNotAvailable,"Geladene Datei enthält keine Liste");
+            }
 
-        // Tipp: Use a directory (ends with "/") to implement a negative test case ;-)
-        // ois = new ObjectInputStream(fis);
+        } catch (FileNotFoundException e) { // Falls die Datei nicht existiert
+            throw new PersistenceException(PersistenceException.ExceptionType.ConnectionNotAvailable,
+                    "Datei nicht gefunden: " + e.getMessage());
 
-        // Reading and extracting the list (try .. catch ommitted here)
-        // Object obj = ois.readObject();
-
-        // if (obj instanceof List<?>) {
-        //       newListe = (List) obj;
-        // return newListe
-
-        // and finally close the streams
-        return null;
+        } catch (IOException | ClassNotFoundException e) {
+            // Andere I/O-Fehler oder Fehler beim Lesen des Objekts
+            throw new PersistenceException(PersistenceException.ExceptionType.ConnectionNotAvailable,
+                    "Fehler beim Laden der Daten: " + e.getMessage());
+        }
     }
 }
