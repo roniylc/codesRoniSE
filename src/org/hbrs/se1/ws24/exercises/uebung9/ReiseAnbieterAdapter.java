c5434bace0f85bbc1bd3a51d5022a98fb75f9449
ronib <roni.yalci@smail.inf.h-brs.de>
2024-12-20 20:45:34 +0100

    Fertige Lösung Übung 9

---
+++ src/org/hbrs/se1/ws24/exercises/uebung9/ReiseAnbieterAdapter.java
@@ -0,0 +1,22 @@
+package org.hbrs.se1.ws24.exercises.uebung9;
+
+public class ReiseAnbieterAdapter implements ExternalHotelSucheInterface{
+
+    private ReiseAnbieter system;
+
+    @Override
+    public SuchErgebnis suche(SuchAuftrag suche){
+        QueryObject q = this.transformInput(suche);
+        QueryResult r = system.executeQuery(q);
+        SuchErgebnis se = this.transformOut(r);
+
+        return se;
+    }
+
+    private QueryObject transformInput(SuchAuftrag s){
+        return null;
+    }
+    private SuchErgebnis transformOut(QueryResult r){
+        return null;
+    }
+}
