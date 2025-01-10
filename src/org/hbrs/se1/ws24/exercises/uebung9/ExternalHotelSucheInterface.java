05e8be80383679a5deddeeb4b5374a9e97bb326b
ronib <roni.yalci@smail.inf.h-brs.de>
2025-01-10 18:59:07 +0100

    Fertige Übung 9

--- src/org/hbrs/se1/ws24/exercises/uebung9/ExternalHotelSucheInterface.java
+++
@@ -1,5 +0,0 @@
-package org.hbrs.se1.ws24.exercises.uebung9;
-
-public interface ExternalHotelSucheInterface {
-    public SuchErgebnis suche(SuchAuftrag s);
-}
--- src/org/hbrs/se1/ws24/exercises/uebung9/ReiseAnbieter.java
+++
@@ -1,8 +0,0 @@
-package org.hbrs.se1.ws24.exercises.uebung9;
-
-public class ReiseAnbieter {
-    public QueryResult executeQuery(QueryObject q){
-        QueryResult r= null;
-        return r;
-    }
-}
